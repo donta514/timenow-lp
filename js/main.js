6ffd91bb861ac305342df7601ea15fec7669ca04
donta514 <58492506+donta514@users.noreply.github.com>
2022-11-14 09:43:54 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('header')
+const menuBtn = document.querySelector('#menu')
+
+menuBtn.addEventListener('click', () => {
+    header.classList.toggle('open')
+})
