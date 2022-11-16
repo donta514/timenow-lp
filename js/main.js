9cdbddbc0a77546fc442d1aa27d632e1cab4253a
donta514 <dnovsmith.ds20@gmail.com>
2022-11-16 17:23:12 -0500

    finalizing pricing page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-const header = document.querySelector('header')
-const menuBtn = document.querySelector('#menu')
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu");
+const sticky = header.offsetTop;
 
-menuBtn.addEventListener('click', () => {
-    header.classList.toggle('open')
-})
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("open");
+});
+
+window.onscroll = () => {
+  stickyHeader();
+};
+
+const stickyHeader = () => {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky-bg");
+  } else {
+    header.classList.remove("sticky-bg");
+  }
+};
