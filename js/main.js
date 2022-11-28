2156767320ce09a7b16521b483775f1864fc214a
donta514 <dnovsmith.ds20@gmail.com>
2022-11-28 17:26:26 -0500

    Added toggle to pricing page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const menuBtn = document.querySelector("#menu");
 const sticky = header.offsetTop;
 const monthlySub = document.querySelector("monthly");
 const yearlySub = document.querySelector("yearly");
+const priceNum = document.querySelector("priceNum");
+const subCount = priceNum.length;
 
 menuBtn.addEventListener("click", () => {
   header.classList.toggle("open");
@@ -21,5 +23,10 @@ const stickyHeader = () => {
 };
 
 monthlySub.addEventListener("click", () => {
-  monthlySub.classList.add("bold");
+  if (monthlySub.classList.contains("bold")) {
+    for (let i = 0; i < subCount; ++i) {
+      const subVal = Number(priceNum[i].innerHTML) * 11;
+      priceNum[i].innerHTML = subVal;
+    }
+  }
 });
