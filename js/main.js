6f942020ba5a39066472b9de614ba7335bf9fe69
donta514 <dnovsmith.ds20@gmail.com>
2022-11-29 16:52:17 -0500

    continued functionality

--- js/main.js
+++ js/main.js
@@ -1,32 +1,45 @@
 const header = document.querySelector("header");
 const menuBtn = document.querySelector("#menu");
 const sticky = header.offsetTop;
-const monthlySub = document.querySelector("monthly");
-const yearlySub = document.querySelector("yearly");
-const priceNum = document.querySelector("priceNum");
+const monthlySub = document.getElementById("monthly");
+const yearlySub = document.getElementById("yearly");
+const priceNum = document.getElementById("priceNum");
 const subCount = priceNum.length;
+const toggle = document.getElementById("toggle");
 
-menuBtn.addEventListener("click", () => {
-  header.classList.toggle("open");
-});
+// menuBtn.addEventListener("click", () => {
+//   header.classList.toggle("open");
+// });
 
-window.onscroll = () => {
-  stickyHeader();
-};
+// window.onscroll = () => {
+//   stickyHeader();
+// };
 
-const stickyHeader = () => {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky-bg");
-  } else {
-    header.classList.remove("sticky-bg");
-  }
-};
+// const stickyHeader = () => {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky-bg");
+//   } else {
+//     header.classList.remove("sticky-bg");
+//   }
+// };
 
-monthlySub.addEventListener("click", () => {
-  if (monthlySub.classList.contains("bold")) {
-    for (let i = 0; i < subCount; ++i) {
-      const subVal = Number(priceNum[i].innerHTML) * 11;
-      priceNum[i].innerHTML = subVal;
-    }
-  }
-});
+// monthlySub.addEventListener("click", () => {
+//   if (monthlySub.classList.contains("bold")) {
+//     for (let i = 0; i < subCount; ++i) {
+//       const subVal = Number(priceNum[i].innerHTML) * 11;
+//       priceNum[i].innerHTML = subVal;
+//     }
+//   }
+// });
+
+// yearlySub.addEventListener("click", () => {
+//   monthlySub.classList.remove("bold");
+//   yearlySub.style.fontWeight = "bold";
+//   const newVal = Number(priceNum.innerHTML) * 11;
+
+// });
+
+
+
+// left: calc(100% - 5px);
+// transform: translateX(-100%);
